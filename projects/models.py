0d568a04cafe0aad8dd64681f3014125a0bd4e82
Faith Mwangi <faith.mwangi@student.moringaschool.com>
2022-06-11 18:47:09 +0300

    add install cloudinary, auth, update settings, urls

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Profile(models.Model):
+  user = models.OmeToOneField(User, related_name='profile')
+  image = CloudinaryField('image')
