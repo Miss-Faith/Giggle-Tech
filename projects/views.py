05e64a0545eacf5c786fcb87bcf0d1f1642b6c68
Faith Mwangi <faith.mwangi@student.moringaschool.com>
2022-06-14 08:37:33 +0300

    add search function and search template

--- projects/views.py
+++ projects/views.py
@@ -6,3 +6,13 @@ def index(request):
 
   return render(request, 'index.html')
 
+@login_required
+def search_results(request):
+    if 'post' in request.GET and request.GET["post"]:
+        search_term = request.GET.get("post")
+        searched_posts = Post.search_post(search_term)
+        message = f"{search_term}"
+        return render(request, 'search.html', {"message":message,"posts": searched_posts})
+    else:
+        message = "You haven't searched for any profile"
+    return render(request, 'search.html', {'message': message})
