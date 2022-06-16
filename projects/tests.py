e5e2812982f6eb8783c5f6a2660c8893ee0f740b
Faith Mwangi <faith.mwangi@student.moringaschool.com>
2022-06-17 00:19:18 +0300

    Add tests

--- projects/tests.py
+++ projects/tests.py
@@ -1,3 +1,56 @@
 from django.test import TestCase
+from .models import *
 
 # Create your tests here.
+class TestProfile(TestCase):
+    def setUp(self):
+        self.user = User(id=1, username='faith', email='faith@gmail.com', password='pass')
+        self.user.save()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.user, User))
+
+    def test_save_user(self):
+        self.user.save()
+
+    def test_delete_user(self):
+        self.user.delete()
+
+
+class PostTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(id=1, username='faith')
+        self.post = Post.objects.create(id=1, title='test post', image='photo.jpg', description='desc', user=self.user, url='http://url.com')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.post, Post))
+
+    def test_save_post(self):
+        self.post.save_post()
+        post = Post.objects.all()
+        self.assertTrue(len(post) > 0)
+
+    def test_get_posts(self):
+        self.post.save()
+        posts = Post.all_posts()
+        self.assertTrue(len(posts) > 0)
+
+    def test_search_post(self):
+        self.post.save()
+        post = Post.search_project('test')
+        self.assertTrue(len(post) > 0)
+
+    def test_delete_post(self):
+        self.post.delete_post()
+        post = Post.search_project('test')
+        self.assertTrue(len(post) < 1)
+
+
+class RatingTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(id=1, username='faith')
+        self.post = Post.objects.create(id=1, title='test post', image='photo.jpg', description='desc', user=self.user, url='http://url.com')
+        self.rating = Rating.objects.create(id=1, design=6, usability=7, content=9, user=self.user, post=self.post)
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.rating, Rating))
